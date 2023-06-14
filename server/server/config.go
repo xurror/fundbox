0765c236b7a580cdefda11c0f2265f8121686838
xurror <kazenasser@gmail.com>
2023-06-14 08:26:49 +0200

    make cors more permissive

--- server/server/config.go
+++ server/server/config.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"getting-to-go/config"
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/secure"
 )
@@ -25,7 +27,17 @@ type RouterConfig struct {
 }
 
 func CorsConfig() cors.Config {
-	return cors.DefaultConfig()
+	return cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+		//AllowOriginFunc: func(origin string) bool {
+		//	return origin == "*"
+		//},
+	}
 }
 
 func SecureConfig() secure.Config {
