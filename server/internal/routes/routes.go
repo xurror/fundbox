4de261c94552e9538034b83cdbf80d0e211e6f74
xurror <kazenasser@gmail.com>
2025-02-18 09:45:12 +0100

    setup swagger documentation

--- server/internal/routes/routes.go
+++ server/internal/routes/routes.go
@@ -4,6 +4,11 @@ import (
 	"community-funds/internal/handlers"
 
 	"github.com/gin-gonic/gin"
+
+	_ "community-funds/docs" // Import Swagger docs
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type Router struct {
@@ -16,6 +21,10 @@ func NewRouter(fundHandler *handlers.FundHandler, userHandler *handlers.UserHand
 }
 
 func (r *Router) SetupRoutes(router *gin.Engine) {
+
+	// Swagger documentation route
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	api := router.Group("/api")
 	{
 		api.GET("/funds", r.FundHandler.GetFunds)
