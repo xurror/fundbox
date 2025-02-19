2ebfe97b455beda4eb728e5d918f62cd785b8c57
xurror <kazenasser@gmail.com>
2025-02-19 09:44:24 +0100

    upgrade to tailwindcss 4

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 
 export default async function Home() {
   const session = await auth0.getSession()
+  if (!session) {
+    console.log("No session found")
+  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
