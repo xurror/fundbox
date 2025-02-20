3cfc55401fee945c6d93914e7040b3ade8115c11
xurror <kazenasser@gmail.com>
2025-02-20 09:00:19 +0100

    implement dashboard with sidebar

---
+++ client/app/dashboard/layout.tsx
@@ -0,0 +1,61 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+	Breadcrumb,
+	BreadcrumbList,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbSeparator,
+	BreadcrumbPage
+} from "@/components/ui/breadcrumb";
+import {
+	SidebarProvider,
+	SidebarInset,
+	SidebarTrigger
+} from "@/components/ui/sidebar";
+import { auth0 } from "@/lib/auth0"
+import { Separator } from "@radix-ui/react-separator";
+import { redirect } from "next/navigation"
+
+export default async function Layout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	const session = await auth0.getSession()
+	if (!session) {
+		return redirect("/login")
+	}
+
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+					<div className="flex items-center gap-2 px-4">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							orientation="vertical"
+							className="mr-2 data-[orientation=vertical]:h-4"
+						/>
+						<Breadcrumb>
+							<BreadcrumbList>
+								<BreadcrumbItem className="hidden md:block">
+									<BreadcrumbLink href="#">
+										Building Your Application
+									</BreadcrumbLink>
+								</BreadcrumbItem>
+								<BreadcrumbSeparator className="hidden md:block" />
+								<BreadcrumbItem>
+									<BreadcrumbPage>Data Fetching</BreadcrumbPage>
+								</BreadcrumbItem>
+							</BreadcrumbList>
+						</Breadcrumb>
+					</div>
+				</header>
+				<>
+					{children}
+				</>
+			</SidebarInset>
+		</SidebarProvider>
+	)
+}
