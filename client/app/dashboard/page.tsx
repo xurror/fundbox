ded9b5e890c5ec88a18f708c58c0a771e5c7ce99
xurror <kazenasser@gmail.com>
2025-02-20 08:21:42 +0100

    wip-dashboard

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -13,15 +13,8 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
-import { auth0 } from "@/lib/auth0"
-import { redirect } from "next/navigation"
 
-export default async function Home() {
-  const session = await auth0.getSession()
-  if (!session) {
-    return redirect("/login")
-  }
-
+export default function Page() {
   return (
     <SidebarProvider>
       <AppSidebar />
